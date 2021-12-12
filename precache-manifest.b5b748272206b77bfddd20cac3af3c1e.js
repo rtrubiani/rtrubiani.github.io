1fdc808ebf7c52148dd99282e30ec1f72123ec69
ritrubia <r.trubiani@gmail.com>
2021-12-12 14:13:47 +0100

    Updates

---
+++ precache-manifest.b5b748272206b77bfddd20cac3af3c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b26f18f04652410a01d4c5d77f00d98",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "b58fdad65677fa8bcfee",
+    "url": "/client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6c871ff1f38be95c2b06",
+    "url": "/client/static/js/2.1941863a.chunk.js"
+  },
+  {
+    "revision": "b58fdad65677fa8bcfee",
+    "url": "/client/static/js/main.4932be3a.chunk.js"
+  },
+  {
+    "revision": "452cefbfebbaabf792a0",
+    "url": "/client/static/js/runtime-main.d5ae527e.js"
+  }
+]);
